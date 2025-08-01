42995d18f784236ebe69c2b9307f1a757b0a1cc4
Harsh Saglani <harshsaglani@gmail.com>
2025-07-31 19:59:17 -0500

    :sparkles: Add delete feature

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react"
 import { useAuth } from "../context/AuthContext"
 import { calculateCoffeeStats, calculateCurrentCaffeineLevel, coffeeConsumptionHistory, getTopThreeCoffees, statusLevels } from "../utils"
+import Modal from "./Modal"
+import { deleteField, doc, updateDoc } from "firebase/firestore"
+import { db } from "../../firebase"
+
 
 function StatCard(props) {
     const { lg, title, children } = props
@@ -12,9 +17,45 @@ function StatCard(props) {
 }
 
 export default function Stats() {
-    const { globalData } = useAuth()
+
+    const [showModal, setShowModal] = useState(false)
+    const [coffeeToDelete, setCoffeeToDelete] = useState(null)
+    const [isDeleting, setIsDeleting] = useState(null)
+
+    const { globalData, setGlobalData, globalUser } = useAuth()
     const stats = calculateCoffeeStats(globalData)
     console.log(stats)
+
+    function handleCloseModal() {
+        setIsDeleting(false)
+        setShowModal(false)
+        setCoffeeToDelete(null)
+    }
+
+    async function handleDeleteCoffee(timestamp) {
+        console.log('Deleting coffee with timestamp:', timestamp)
+        if (!timestamp || isDeleting) {
+            return
+        }
+
+        try {
+            setIsDeleting(true)
+            const newGlobalData = { ...(globalData || {}) }
+            delete newGlobalData[timestamp]
+            setGlobalData(newGlobalData)
+
+            const userRef = doc(db, 'users', globalUser.uid)
+
+            const res = await updateDoc(userRef, {
+                [timestamp]: deleteField()
+            })
+
+            handleCloseModal()
+        } catch (err) {
+            console.error(err.message)
+        }
+
+    }
 
     const caffeineLevel = calculateCurrentCaffeineLevel(globalData)
     const warningLevel = caffeineLevel < statusLevels['low'].maxLevel ?
@@ -25,6 +66,20 @@ export default function Stats() {
 
     return (
         <>
+            {showModal && (
+                <Modal handleCloseModal={handleCloseModal}>
+                    <div className="delete-modal">
+                        <h2 className="sign-up-text" >Do you want to delete this coffee?</h2>
+                        <div className="button-wrapper coffee-grid">
+                            <button onClick={() => {
+                                handleDeleteCoffee(coffeeToDelete)
+                                // handleCloseModal()
+                            }} style={{ color: "#f35353" }}>{!isDeleting ? 'Yes' : 'Deleting...'}</button>
+                            <button onClick={handleCloseModal}>No</button>
+                        </div>
+                    </div>
+                </Modal>
+            )}
             <div className="section-header">
                 <i className="fa-solid fa-chart-simple" />
                 <h2>Stats</h2>
@@ -55,15 +110,21 @@ export default function Stats() {
                             <th>Coffee Name</th>
                             <th>Number of Purchase</th>
                             <th>Percentage of Total</th>
+                            <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
                         {getTopThreeCoffees(globalData).map((coffee, coffeeIndex) => {
+                            // console.log("coffee", Object.keys(globalData)[coffeeIndex])
                             return (
                                 <tr key={coffeeIndex}>
                                     <td>{coffee.coffeeName}</td>
                                     <td>{coffee.count}</td>
                                     <td>{coffee.percentage}</td>
+                                    <td><button onClick={() => {
+                                        setShowModal(true)
+                                        setCoffeeToDelete(Object.keys(globalData)[coffeeIndex])
+                                    }}><i className="fa-solid fa-trash" style={{ color: "#f35353" }}></i></button></td>
                                 </tr>
                             )
                         })}
